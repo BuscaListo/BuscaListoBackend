5b4f66789e587e7ccba6e0486372880ea7c0ff08
Miguel Andres Herize Godoy <mherize@ia.com.mx>
2025-08-19 23:00:10 -0400

    add minor change port and ip

--- app/infrastructure/config/db_config.py
+++ app/infrastructure/config/db_config.py
@@ -5,12 +5,12 @@ class DBSettings(BaseSettings):
     USERDB: str
     PASSWORDDB: str
     NAME_SERVICEDB: str
-    PORT: int
+    PORTDB: str
     NAMEDB: str
 
     @property
     def DATABASE_URL(self) -> str:
-        return f"{self.DB}://{self.USERDB}:{self.PASSWORDDB}@{self.NAME_SERVICEDB}:{self.PORT}/{self.NAMEDB}"
+        return f"{self.DB}://{self.USERDB}:{self.PASSWORDDB}@{self.NAME_SERVICEDB}:{self.PORTDB}/{self.NAMEDB}"
 
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import os
+import httpx
+import socket
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.infrastructure import session  
 
@@ -21,17 +23,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 @app.get("/")
-async def root():
+async def root(request: Request):
     """
-    Endpoint básico de bienvenida.
+    Endpoint básico que devuelve información del proyecto + IPs.
     """
+    client_host = request.headers.get("x-forwarded-for") or request.client.host
+    server_ip = socket.gethostbyname(socket.gethostname())
+
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://api.ipify.org?format=json")
+        public_ip = r.json()["ip"]
+
     return {
         "project": PROJECT_NAME,
         "version": VERSION,
         "status": "running",
-        "db_type": os.getenv("DB_TYPE", "sqlite")
+        "db_type": os.getenv("DB_TYPE", "sqlite"),
+        "client_ip": client_host,
+        "server_ip": server_ip,
+        "public_ip": public_ip
     }
 
 # Incluir routers (ejemplo)
